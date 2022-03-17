7311c12b4fcd16534f6d7535d90f2a8697ddeb66
cedricChopin <72026207+cedricChopin@users.noreply.github.com>
2022-03-17 21:43:00 +0100

    SnaxMountain added + MAP SAVED

---
+++ Source/PoroBot/SnaxMountain.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SnaxMountain.h"
+
+ASnaxMountain::ASnaxMountain() {
+    OnActorBeginOverlap.AddDynamic(this, &ASnaxMountain::OverlapBegin);
+}
+
+void ASnaxMountain::BeginPlay() {
+    Super::BeginPlay();
+    DrawDebugBox(GetWorld(), GetActorLocation(), GetComponentsBoundingBox().GetExtent(), FColor::Red, true, -1, 0, 5);
+}
+
+void ASnaxMountain::OverlapBegin(AActor* OverlappedComponent, AActor* OtherActor)
+{
+    if (OtherActor && (OtherActor != this)) {
+        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "PLEINS DE POROSNAX");
+
+        //UComboWidget::WinPoro();
+    }
+}
---
+++ Source/PoroBot/SnaxMountain.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ComboWidget.h"
+#include "Engine/TriggerBox.h"
+#include "SnaxMountain.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class POROBOT_API ASnaxMountain : public ATriggerBox
+{
+	GENERATED_BODY()
+		ASnaxMountain();
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OverlapBegin(class AActor* OverlappedComp, class AActor* OtherActor);
+};
